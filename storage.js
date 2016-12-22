af1da449dfecbb8b4673d0dc7b7f41e0c5bf7c3e
Alistair Hastings <alistairhastings@yahoo.com>
2016-12-22 09:46:07 -0500

    Add storage.js and storage.tests.js

---
+++ storage.js
@@ -0,0 +1,81 @@
+﻿/****************************************************/
+//
+// Library that wraps sessionStorage for easier use
+//
+// Uses the same interface as sessionStorage, but
+// handles conversion from JavaScript objects to JSON
+// and provides a polyfill for older browsers.
+//
+/****************************************************/
+
+var storage = function storage(theWindow) {
+    // storagePolyfill will be used in instances where sessionStorage
+    // is unavailable (mostly moderately old versions of IE, or 
+    // any IE when the script is invoked from local filesystem).
+    // It provides a similar interface as sessionStorage, so
+    // consumers of this library need not worry about browser version.
+    var storagePolyfill = function () {
+        // Use a simple object instead of sessionStorage
+        // It won't be saved throughout the session, but will 
+        // allow the script to run without error.
+        var items = {};
+
+        return {
+            getItem: function (key) {
+                var item = items[key];
+
+                // Invalid reference returns undefined, not null,
+                // but sessionStorage spec requires return of null.
+                return typeof item === "undefined" ? null : item;
+            },
+            setItem: function (key, value) {
+                items[key] = value;
+            },
+            removeItem: function (key) {
+                delete items[key];
+            }
+        }
+    }();
+
+    // Ensure sessionStorage is present and defined (it won't be on older IE, or IE running script from local file system)
+    // If not, use the polyfill
+    var storageAdaptor = 'sessionStorage' in theWindow && typeof theWindow.sessionStorage !== "undefined" ? theWindow.sessionStorage : storagePolyfill;
+
+    // Simple function to return the type of an object (more robust/accurate than "typeof")
+    var toType = function (obj) {
+        return ({}).toString.call(obj).match(/\s([a-z|A-Z]+)/)[1].toLowerCase();
+    };
+
+
+    // Return an object that mimics the interface to sessionStorage
+    return {
+        getItem: function (key) {
+            var item = storageAdaptor.getItem(key);
+
+            try {
+                item = JSON.parse(item);
+            } catch (e) {
+                // parse is expected to fail on some types, ignore this and return anyway.
+            }
+
+            return item;
+        },
+
+        setItem: function (key, value) {
+            // Get the type of value
+            var type = toType(value);
+
+            // If value is an object or array, stringify it for storage,
+            // as sessionStorage only accepts strings 
+            if (/object|array/.test(type)) {
+                value = JSON.stringify(value);
+            }
+
+            storageAdaptor.setItem(key, value);
+        },
+
+        removeItem: function (key) {
+            storageAdaptor.removeItem(key);
+        }
+    };
+}(this); // Pass in the global object (window, in browsers, something else in Node).
---
+++ storage.tests.js
@@ -0,0 +1,79 @@
+﻿describe("storage", function () {
+    beforeEach(function () {
+        if ('sessionStorage' in window && typeof window.sessionStorage !== "undefined") {
+            sessionStorage.clear();
+        }
+    });
+
+    describe("Setting", function () {
+
+        it(":storage sets primitives", function () {
+            var number = 3;
+            var string = "3 is three";
+
+            storage.setItem("test1", number);
+            storage.setItem("test2", string)
+
+            expect(storage.getItem("test1")).toEqual(number);
+            expect(storage.getItem("test2")).toEqual(string);
+        });
+
+        it(":storage sets object", function () {
+            data = { number: 3 };
+            storage.setItem("test", data);
+
+            expect(storage.getItem("test")).toEqual(data);
+        });
+
+        it(":storage sets array", function () {
+            data = [1, 2, 3];
+            storage.setItem("test", data);
+
+            expect(storage.getItem("test")).toEqual(data);
+        });
+    });
+
+    describe("Getting", function () {
+        it(":storage gets primitives", function () {
+            var number = 3;
+            var string = "3 is three";
+
+            storage.setItem("test1", number);
+            storage.setItem("test2", string)
+
+            expect(storage.getItem("test1")).toEqual(number);
+            expect(storage.getItem("test2")).toEqual(string);
+        });
+
+        it(":storage gets object", function () {
+            data = { number: 3, character: 'a' };
+            storage.setItem("test", data);
+
+            expect(storage.getItem("test")).toEqual(jasmine.any(Object));
+            expect(storage.getItem("test").number).toEqual(data.number);
+            expect(storage.getItem("test").character).toEqual(data.character);
+        });
+
+        it(":storage gets array", function () {
+            data = [1, '2', 3];
+            storage.setItem("test", data);
+
+            expect(storage.getItem("test")).toEqual(jasmine.any(Array));
+            expect(storage.getItem("test")[0]).toEqual(data[0]);
+            expect(storage.getItem("test")[1]).toEqual(data[1]);
+            expect(storage.getItem("test")[2]).toEqual(data[2]);
+        });
+    });
+
+    describe("Removing", function () {
+        it(":storage removes item", function () {
+            data = 3;
+
+            storage.setItem("test", data);
+            expect(storage.getItem("test")).toEqual(data);
+
+            storage.removeItem("test");
+            expect(storage.getItem("test")).toEqual(null);
+        });
+    });
+});
